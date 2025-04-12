09d9d7e95d57d3654c5806121eff20db180bae4b
20fachri <fachrigame123@gmail.com>
2025-04-12 07:39:37 +0700

    Add files via upload

---
+++ styles/Jalan_Section_3_style.js
@@ -0,0 +1,49 @@
+var size = 0;
+var placement = 'point';
+function categories_Jalan_Section_3(feature, value, size, resolution, labelText,
+                       labelFont, labelFill, bufferColor, bufferWidth,
+                       placement) {
+                var valueStr = (value !== null && value !== undefined) ? value.toString() : 'default';
+                switch(valueStr) {case '1':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(123,232,68,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 2.508}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case '2':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(32,97,218,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 2.508}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;}};
+
+var style_Jalan_Section_3 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    
+    var labelText = ""; 
+    var value = feature.get("Urutan");
+    var labelFont = "13.0px \'Open Sans\', sans-serif";
+    var labelFill = "#323232";
+    var bufferColor = "#fafafa";
+    var bufferWidth = 3.0;
+    var textAlign = "left";
+    var offsetX = 0;
+    var offsetY = 0;
+    var placement = 'line';
+    if (feature.get("Urutan") !== null) {
+        labelText = String(feature.get("Urutan"));
+    }
+    
+    var style = categories_Jalan_Section_3(feature, value, size, resolution, labelText,
+                            labelFont, labelFill, bufferColor,
+                            bufferWidth, placement);
+
+    return style;
+};
